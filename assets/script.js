36f6e29eb2e70e94a1fdea42d982d6c37f6d2961
Kai Dong <dongkai981@gmail.com>
2020-11-02 21:16:07 -0600

    Mocked up layout for html, started on js with some variables and pseudocode

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+// Variables
+let myUrl = "https://api.openweathermap.org/data/2.5/"
+let apiKey = "0c2de5fc4c08fa9a0a103d78dbde557c"
+
+// Local Storage for search history
+
+// Search functionality
+    // 
+// Current Weather
+    // Detailed weather output to html
+// Five Day Weather
+    // 
