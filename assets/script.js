2db561cdd24458e56eaf3b4fc5fa5a7113d3281f
Kai Dong <dongkai981@gmail.com>
2020-11-04 20:22:54 -0600

    Added logic for local storage of city names input

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,13 @@ let myUrl = "https://api.openweathermap.org/data/2.5/"
 let apiKey = "0c2de5fc4c08fa9a0a103d78dbde557c"
 
 // Local Storage for search history
-
+for (let i = 0; i < localStorage.length; i++) {
+    let city = localStorage.getItem(i);
+    // Add class for the list
+    let cityName = $(".list-group").addClass("list-group-item");
+    // Append city name as a list
+    cityName.append("<li>" + city + "</li>");
+}
 // Search functionality
     // 
 // Current Weather
